c3858e219311933647460f0d5ab8a3b7d1689da5
hue.dongho.park <hue.dongho.park@gmail.com>
2016-05-10 17:48:35 +0900

    RatingStar component added

---
+++ Xamarin.Forms-Components/Views/RatingStarsPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Xamarin.FormsComponents
+{
+	public partial class RatingStarsPage : ContentPage
+	{
+		ViewModel viewModel { get; set; }
+
+		public RatingStarsPage ()
+		{
+			InitializeComponent ();
+
+			viewModel = new ViewModel ();
+			BindingContext = viewModel;
+		}
+	}
+
+	class ViewModel : ViewModelBase
+	{
+		int rating { get; set; }
+
+		public int Rating {
+			get {
+				return rating;
+			}
+			set {
+				if (rating != value) {
+					rating = value;
+					RaisePropertyChanged ();
+				}
+			}
+		}
+	}
+
+
+}
+
---
+++ Xamarin.Forms-Components/Views/ViewListPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace Xamarin.FormsComponents
+{
+	public partial class ViewListPage : ContentPage
+	{
+		ViewModel viewModel { get; set; }
+
+		public ViewListPage ()
+		{
+			InitializeComponent ();
+
+			viewModel = new ViewModel (); 
+			BindingContext = viewModel;
+
+			viewModel.Views.Add (new ItemViewModel () { 
+				Text = "RatingStars", 
+				Detail = "Readonly RatingStars Component", 
+				PageClassName = "RatingStarsPage"
+			});
+
+			// Add your page here..
+		}
+
+		public async void OnItemSelected (object sender, SelectedItemChangedEventArgs args)
+		{
+			ItemViewModel itemViewModel = (ItemViewModel)(args.SelectedItem);
+			if (null != itemViewModel) {
+				((ListView)sender).SelectedItem = null;
+				if (null != itemViewModel.PageClassName) {
+					Type t = Type.GetType ("Xamarin.FormsComponents." + itemViewModel.PageClassName); 
+					if (null != t) {
+						ContentPage page = (ContentPage)Activator.CreateInstance (t);
+						if (null != page) {
+							await this.Navigation.PushAsync (page);
+						}
+					}
+				}
+			}
+		}
+
+		class ViewModel : ViewModelBase
+		{
+			public ObservableCollection<ItemViewModel> Views { get; private set; }
+
+			public ViewModel ()
+			{
+				Views = new ObservableCollection<ItemViewModel> ();
+			}
+		}
+
+		class ItemViewModel : ViewModelBase
+		{
+			public string Text { get; set; }
+
+			public string Detail { get; set; }
+
+			public string PageClassName  { get; set; }
+		}
+	}
+}
